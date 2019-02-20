f641b90f40f0e3c8595ab7c7711ef37118f1c52a
Vladimir <always-optimus@yandex.ru>
2019-02-20 20:56:23 +0300

    3-й день урока

--- js/script.js
+++ js/script.js
@@ -1,114 +1,154 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    const loadContent = async (url, callback) => {
+      await fetch(url) // Promise 
+        .then(response => response.json()) // Обещание, превращает json формата в обычный js
+        .then(json => createElement(json.goods));
 
-  const cartWrapper = document.querySelector('.cart__wrapper'),
-    cart = document.querySelector('.cart'),
-    close = document.querySelector('.cart__close'),
-    open = document.querySelector('#cart'),
-    goodsBtn = document.querySelectorAll('.goods__btn'),
-    products = document.querySelectorAll('.goods__item'),
-    confirm = document.querySelector('.confirm'),
-    badge = document.querySelector('.nav__badge'),
-    totalCost = document.querySelector('.cart__total > span'),
-    titles = document.querySelectorAll('.goods__title'),
-    empty = cartWrapper.querySelector('.empty');
+      callback();
+    }
 
-  function openCart() {
-    cart.style.display = 'block';
-    document.body.style.overflow = 'hidden';
-  }
+    function createElement(arr) {
+      const goodsWrapper = document.querySelector('.goods__wrapper');
 
-  function closeCart() {
-    cart.style.display = 'none';
-    document.body.style.overflow = '';
-  }
-
-  open.addEventListener('click', openCart);
-  close.addEventListener('click', closeCart);
-
-  goodsBtn.forEach(function (btn, i) {
-    btn.addEventListener('click', () => {
-      let item = products[i].cloneNode(true),
-        trigger = item.querySelector('button'),
-        removeBtn = document.createElement('div');
-
-      trigger.remove();
-
-      showConfirm();
-      calcGoods(1);
-
-      removeBtn.classList.add('goods__item-remove');
-      removeBtn.innerHTML = '&times';
-      item.appendChild(removeBtn);
-
-      cartWrapper.appendChild(item);
-      if (empty) {
-        empty.remove();
-      }
-      calcTotal();
-      removeFromCart();
-    });
-  });
-
-  function sliceTitle() {
-    titles.forEach(function (item) {
-      if (item.textContent.length < 70) {
-        return;
-      } else {
-        const str = item.textContent.slice(0, 71) + '...';
-        //  const str = `${item.textContent.slice(0, 71)}...`;
-        item.textContent = str;
-      }
-    });
-  }
-  sliceTitle();
-
-  function showConfirm() {
-    confirm.style.display = 'block';
-    let counter = 100;
-    const id = setInterval(frame, 10);
-    function frame() {
-      if ( counter == 10) {
-          clearInterval(id);
-          confirm.style.display = 'none';
-      } else {
-        counter--;
-        confirm.style.transform = `translateY(-${counter}px)`;
-        confirm.style.opacity = '.' + counter;
-      }
-  }
-  }
-
-  function calcGoods(i) {
-    const items = cartWrapper.querySelectorAll('.goods__item');
-    badge.textContent = i + items.length;
-  }
-
-  function calcTotal() {
-    const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
-    let total = 0;
-    prices.forEach(function(item) {
-      total += +item.textContent;
-    });
-    totalCost.textContent = total;
-    return total;
-  };
-
-  
-  function removeFromCart () {
-    const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
-    removeBtn.forEach(function(btn) {
-      btn.addEventListener('click', () => {
-        btn.parentElement.remove();
-        calcGoods(0);
-        calcTotal();
-        if (calcTotal () === 0) {
-          empty.style.display = 'block';
-        } 
+      arr.forEach(function (item) {
+        let card = document.createElement('div');
+        card.classList.add('goods__item');
+        card.innerHTML = `
+          <img class = "goods__img" src = "${item.url}" alt = "phone">
+            <div class = "goods__colors"> Доступно цветов: 4 </div> 
+            <div class = "goods__title">
+              ${item.title}
+            </div> 
+            <div class = "goods__price">
+            <span >${item.price}< /span> руб/шт </div> 
+            <button class = "goods__btn"> Добавить в корзину </button> 
+            </div>
+      `;
+        goodsWrapper.appendChild(card);
       });
+    }
+
+    loadContent('js/db.json', () => {
+      const cartWrapper = document.querySelector('.cart__wrapper'),
+        cart = document.querySelector('.cart'),
+        close = document.querySelector('.cart__close'),
+        open = document.querySelector('#cart'),
+        goodsBtn = document.querySelectorAll('.goods__btn'),
+        products = document.querySelectorAll('.goods__item'),
+        confirm = document.querySelector('.confirm'),
+        badge = document.querySelector('.nav__badge'),
+        totalCost = document.querySelector('.cart__total > span'),
+        titles = document.querySelectorAll('.goods__title'),
+        empty = cartWrapper.querySelector('.empty');
+
+      function openCart() {
+        cart.style.display = 'block';
+        document.body.style.overflow = 'hidden';
+      }
+
+      function closeCart() {
+        cart.style.display = 'none';
+        document.body.style.overflow = '';
+      }
+
+      open.addEventListener('click', openCart);
+      close.addEventListener('click', closeCart);
+
+      goodsBtn.forEach(function (btn, i) {
+        btn.addEventListener('click', () => {
+          let item = products[i].cloneNode(true),
+            trigger = item.querySelector('button'),
+            removeBtn = document.createElement('div'),
+            empty = cartWrapper.querySelector('.empty');
+
+          trigger.remove();
+
+          showConfirm();
+          calcGoods(1);
+
+          removeBtn.classList.add('goods__item-remove');
+          removeBtn.innerHTML = '&times';
+          item.appendChild(removeBtn);
+
+          cartWrapper.appendChild(item);
+          if (empty) {
+            empty.remove();
+          }
+          calcTotal();
+          removeFromCart();
+        });
+      });
+
+      function sliceTitle() {
+        titles.forEach(function (item) {
+          if (item.textContent.length < 70) {
+            return;
+          } else {
+            const str = item.textContent.slice(0, 70) + '...';
+            //  const str = `${item.textContent.slice(0, 71)}...`;
+            item.textContent = str;
+          }
+        });
+      }
+      sliceTitle();
+
+      function showConfirm() {
+        confirm.style.display = 'block';
+        let counter = 100;
+        const id = setInterval(frame, 10);
+
+        function frame() {
+          if (counter == 10) {
+            clearInterval(id);
+            confirm.style.display = 'none';
+          } else {
+            counter--;
+            confirm.style.transform = `translateY(-${counter}px)`;
+            confirm.style.opacity = '.' + counter;
+          }
+        }
+      }
+
+      function calcGoods(i) {
+        const items = cartWrapper.querySelectorAll('.goods__item');
+        badge.textContent = i + items.length;
+      }
+
+      function calcTotal() {
+        const prices = document.querySelectorAll('.cart__wrapper > .goods__item > .goods__price > span');
+        let total = 0;
+        prices.forEach(function (item) {
+          total += +item.textContent;
+        });
+        totalCost.textContent = total;
+        return total;
+      };
+
+
+      function removeFromCart() {
+        const removeBtn = cartWrapper.querySelectorAll('.goods__item-remove');
+        removeBtn.forEach(function (btn) {
+          btn.addEventListener('click', () => {
+            btn.parentElement.remove();
+            calcGoods(0);
+            calcTotal();
+
+            if (cartWrapper.querySelectorAll('.goods__item').length == 0) {
+              cartWrapper.querySelector('.empty').style.display = 'block';
+            }
+
+          });
+        });
+      }
     });
-  };  
-  // setInterval(sliceTitle, 100)
-  // setTimeout(sliceTitle, 100)
 
 });
+
+
+// fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: "POST",
+//     body: JSON.stringify(example)
+// }) // Обещание
+//   .then(response => response.json()) // Обещание, превращает json формата в обычный js
+//   .then(json => console.log(json))
